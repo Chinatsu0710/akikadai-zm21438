10ba9d8fd9c114d0be7041d9a6bff615f1d83ced
Chinatsu0710 <131092090+Chinatsu0710@users.noreply.github.com>
2023-12-19 10:23:28 +0900

    Update 1-1.js

--- 1-1.js
+++ 1-1.js
@@ -1,1 +1,1 @@
-function gazo1(){document.getElementById('who1').innerHTML='<img src="image/pyoko12_christmas_wreath"alt="オン">';}
+function gazo1(){document.getElementById('who1').innerHTML='<img src="image/pyoko12_christmas_wreath.png"alt="オン">';}
